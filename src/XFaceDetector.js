ff5a46f766ee36d608645538560b65e40fece4e1
Karl Herrick <me@karlherrick.com>
2020-02-28 19:34:45 -0500

    remove async / await

--- src/XFaceDetector.js
+++ src/XFaceDetector.js
@@ -70,56 +70,57 @@ export class XFaceDetector extends LitElement {
     `
   }
 
-  async _drawPrediction(ctx, image) {
+  _drawPrediction(ctx, image) {
     // Pass in an image or video to the model. The model returns an array of
     // bounding boxes, probabilities, and landmarks, one for each detected face.
     const returnTensors = false // Pass in `true` to get tensors back, rather than values.
-    const predictions = await this.model.estimateFaces(image, returnTensors)
 
-    if (predictions.length > 0) {
-      /*
-      `predictions` is an array of objects describing each detected face, for example:
-      [
-        {
-          topLeft: [232.28, 145.26],
-          bottomRight: [449.75, 308.36],
-          probability: [0.998],
-          landmarks: [
-            [295.13, 177.64], // right eye
-            [382.32, 175.56], // left eye
-            [341.18, 205.03], // nose
-            [345.12, 250.61], // mouth
-            [252.76, 211.37], // right ear
-            [431.20, 204.93]  // left ear
-          ]
+    return this.model.estimateFaces(image, returnTensors).then(predictions => {
+      if (predictions.length > 0) {
+        /*
+        `predictions` is an array of objects describing each detected face, for example:
+        [
+          {
+            topLeft: [232.28, 145.26],
+            bottomRight: [449.75, 308.36],
+            probability: [0.998],
+            landmarks: [
+              [295.13, 177.64], // right eye
+              [382.32, 175.56], // left eye
+              [341.18, 205.03], // nose
+              [345.12, 250.61], // mouth
+              [252.76, 211.37], // right ear
+              [431.20, 204.93]  // left ear
+            ]
+          }
+        ]
+        */
+
+        ctx.strokeStyle = this.strokeStyle
+        ctx.lineWidth = this.lineWidth
+
+        for (let i = 0; i < predictions.length; i++) {
+          const start = predictions[i].topLeft
+          const end = predictions[i].bottomRight
+          const size = [end[0] - start[0], end[1] - start[1]]
+
+          const rectangle = [start[0], start[1], size[0], size[1]]
+
+          logger([ 'Face detected', rectangle ])
+          this.dispatchEvent(events.XFaceDetectorFaceDetected(rectangle))
+
+          // Render a rectangle over each detected face.
+          ctx.strokeRect(...rectangle)
         }
-      ]
-      */
 
-      ctx.strokeStyle = this.strokeStyle
-      ctx.lineWidth = this.lineWidth
-
-      for (let i = 0; i < predictions.length; i++) {
-        const start = predictions[i].topLeft
-        const end = predictions[i].bottomRight
-        const size = [end[0] - start[0], end[1] - start[1]]
-
-        const rectangle = [start[0], start[1], size[0], size[1]]
-
-        logger([ 'Face detected', rectangle ])
-        this.dispatchEvent(events.XFaceDetectorFaceDetected(rectangle))
-
-        // Render a rectangle over each detected face.
-        await ctx.strokeRect(...rectangle)
+        return [ ctx, image ]
       }
 
+      logger('No Face detected')
+      this.dispatchEvent(events.XFaceDetectorNoFaceDetected())
+
       return [ ctx, image ]
-    }
-
-    logger('No Face detected')
-    this.dispatchEvent(events.XFaceDetectorNoFaceDetected())
-
-    return [ ctx, image ]
+    })
   }
 
   _getImage(url) {
